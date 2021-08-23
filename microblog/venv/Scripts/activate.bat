cb25b3ee9c77346dd31357c58a6668a891406cbe
[Cassie Ritchie] <[22358769@student.uwa.edu.au]>
2021-08-23 21:38:02 +0800

    Finished ch 8

--- microblog/venv/Scripts/activate.bat
+++ microblog/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\casan\Desktop\microblog\venv"
+set "VIRTUAL_ENV=C:\Users\casan\Desktop\Plaque\Commemorative-Plaques-of-UWA\microblog\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
