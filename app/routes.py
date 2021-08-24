7809ef9b0c3d81f9e1d4af04f39049b3ae9824da
eddie485 <ervin25basilio@gmail.com>
2021-08-24 10:53:51 +0800

    Changed the layout from flask tutorial to actual plaques website

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template, flash, redirect, url_for
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/locations")
+def locations():
+    return render_template("locations.html", title="Locations")
+
+if __name__=="__main__":
+    app.run(debug=True)
