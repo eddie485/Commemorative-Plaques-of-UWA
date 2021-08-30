541cd13af256a885487594192be2fac7e1b73a3e
timpye <72972212+timpye@users.noreply.github.com>
2021-08-30 11:55:17 +0800

    Created Locations template base
    Locations page now accepts input from routes.

--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,24 @@ def index():
 def about():
     return render_template("about.html", title="About")
 
+#Creating a locations class for debugging purposes before the database is created
+class Location:
+    def __init__(self):
+        self.Name = "Winthrop Hall"
+        self.Description = "Winthrop Hall is a nice place"
+        self.Type = "Building"
+        self.Image_Path = "{{ url_for('static', filename='images/winthrophall1.jpg')}}"
+
+locations_list = [Location()]
+for location in locations_list:
+    print(location.Name)
+    print(location.Description)
+    print(location.Type)
+    print(location.Image_Path)
 @app.route("/locations")
 def locations():
-    return render_template("locations.html", title="Locations")
+    return render_template("locations.html", title="Locations",
+    locations=locations_list)
 
 if __name__=="__main__":
     app.run(debug=True)
