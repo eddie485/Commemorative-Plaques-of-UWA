809b86ad16d65f07247928e13df90115e82eeb70
timpye <72972212+timpye@users.noreply.github.com>
2021-09-02 10:57:44 +0800

    Added database migration

--- app/routes.py
+++ app/routes.py
@@ -11,21 +11,9 @@ def index():
 def about():
     return render_template("about.html", title="About")
 
-#Creating a locations class for debugging purposes before the database is created
-"""class Location:
-    def __init__(self):
-        self.Name = "Winthrop Hall"
-        self.Description = "Winthrop Hall is a nice place"
-        self.Type = "Building"
-        self.Image_Path = "{{ url_for('static', filename='images/winthrophall1.jpg')}}"
 
-locations_list = [Location()]
-for location in locations_list:
-    print(location.Name)
-    print(location.Description)
-    print(location.Type)
-    print(location.Image_Path)"""
-    
+
+
 @app.route("/locations")
 def locations():
     return render_template("locations.html", title="Locations",
