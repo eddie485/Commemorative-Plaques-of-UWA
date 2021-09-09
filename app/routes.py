fca63075a81ee69b990aece9d6b8fca71c1e61a0
eddie485 <ervin25basilio@gmail.com>
2021-09-09 23:20:15 +0800

    added buttons, new homepage, new html for each theme, small changes to style

--- app/routes.py
+++ app/routes.py
@@ -16,5 +16,13 @@ def locations():
     return render_template("locations.html", title="Locations",
     locations=json.dumps(locations_list))
 
+@app.route("/theme1")
+def walk1():
+    return render_template("theme1.html", title="Theme 1")
+
+@app.route("/theme2")
+def walk2():
+    return render_template("theme2.html", title="Theme 2")
+
 if __name__=="__main__":
     app.run(debug=True)
