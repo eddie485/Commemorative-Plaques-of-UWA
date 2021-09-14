d2c63b7c0ea5e4abb64a1b71254dedd59e6dd78c
JamieVo890 <jamievo123@gmail.com>
2021-09-14 04:59:20 -0700

    fixed issue markers not appearing on index

--- app/static/scripts/walk1.js
+++ app/static/scripts/walk1.js
@@ -1,24 +1,6 @@
 var color1 = "#27348b"
 
-// extend mapboxGL class so we can edit the click function
-class CustomMarker extends mapboxgl.Marker {
-  // new method onClick, sets _handleClick to a function you pass in
-  onClick(handleClick) {
-    this._handleClick = handleClick;
-    return this;
-  }
 
-  // the existing _onMapClick was there to trigger a popup
-  // but we are hijacking it to run a function we define
-  _onMapClick(e) {
-    const targetElement = e.originalEvent.target;
-    const element = this._element;
-
-    if (this._handleClick && (targetElement === element || element.contains((targetElement)))) {
-      this._handleClick();
-    }
-  }
-};
 
 
 new CustomMarker({ color: color1 })
--- app/static/scripts/walk2.js
+++ app/static/scripts/walk2.js
@@ -1,26 +1,5 @@
 var color2 = "#e2b600"
 
-// extend mapboxGL class so we can edit the click function
-    class CustomMarker extends mapboxgl.Marker {
-      // new method onClick, sets _handleClick to a function you pass in
-      onClick(handleClick) {
-        this._handleClick = handleClick;
-        return this;
-      }
-
-      // the existing _onMapClick was there to trigger a popup
-      // but we are hijacking it to run a function we define
-      _onMapClick(e) {
-        const targetElement = e.originalEvent.target;
-        const element = this._element;
-
-        if (this._handleClick && (targetElement === element || element.contains((targetElement)))) {
-          this._handleClick();
-        }
-      }
-    };
-
-
 new CustomMarker({ color: color2 })
         .setLngLat([115.817813, -31.978938])
         .onClick(() => { //when clicked, define the following function
