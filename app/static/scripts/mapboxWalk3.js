6dfa1a771a68991114e3bb7863799fae21d6dc2a
timpye <72972212+timpye@users.noreply.github.com>
2021-10-05 10:49:08 +0800

    added automatic geolocate to walks

--- app/static/scripts/mapboxWalk3.js
+++ app/static/scripts/mapboxWalk3.js
@@ -71,4 +71,7 @@ document.addEventListener("DOMContentLoaded", function(){
             map.setStyle('mapbox://styles/mapbox/' + layerId);
         };
     };
+    setTimeout(function() {
+        $(".mapboxgl-ctrl-geolocate").click();
+    },5000);
 });
